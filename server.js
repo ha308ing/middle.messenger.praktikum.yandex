41d0b7f3e634e42ea54d2ed9bc3a6555adecd294
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2023-10-30 21:00:27 +0400

    server: accept env port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const distDir = resolve(__dirname, "dist");
 
 app.use(express.static(distDir));
