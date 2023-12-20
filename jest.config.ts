c72da4c5183f292aaa82202bea4875fb0cb40b33
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2023-12-20 17:05:29 +0400

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  verbose: true,
+  silent: true,
+  preset: "ts-jest",
+  testRegex: "/src/system/.*.test.ts",
+  testEnvironment: "jsdom",
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        // ts-jest configuration goes here
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+export default jestConfig;
